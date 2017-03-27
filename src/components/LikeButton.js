c08b352d79b01af2e64e06deb60f4e57a6aeeb57
Athivvat <athivvat@gmail.com>
2017-03-27 18:13:07 +0700

    Initial setup project

---
+++ src/components/LikeButton.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class LikeButton extends Component {
+  render() {
+    return (
+      <h>React Like Button</h>
+    )
+  }
+}
+
+export default LikeButton;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import LikeButton from './components/LikeButton'
+
+ReactDOM.render(<LikeButton />, document.getElementById('app'));
